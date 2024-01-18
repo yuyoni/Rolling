23fe7907e17158a9ad30d0c7af0ce96e6f0004a1
yuyoni <doli0116@naver.com>
2024-01-18 18:20:35 +0900

    Design : 컴포넌트 추가 및 레이아웃 변경
    - Paper 컴포넌트 생성 및 PaperContainer 컴포넌트에 추가
    - PaperContainer 스타일 추가

--- src/components/Paper/PaperContainer.jsx
+++ src/components/Paper/PaperContainer.jsx
@@ -1,3 +1,16 @@
+import * as S from './PaperContainer.style';
+import Paper from './Paper';
+
 export default function PaperContainer() {
-  return <div>PaperContainer</div>;
+  return (
+    <S.Wrapper>
+      <S.SlideContainer>
+        {/* 임시로 직접 4개 불러와봄 */}
+        <Paper />
+        <Paper />
+        <Paper />
+        <Paper />
+      </S.SlideContainer>
+    </S.Wrapper>
+  );
 }
---
+++ src/components/Paper/PaperContainer.style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div``;
+
+export const SlideContainer = styled.div`
+  display: flex;
+  gap: 20px;
+  max-width: 1160px;
+  overflow: hidden;
+`;
