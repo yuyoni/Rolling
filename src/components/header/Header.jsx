1dea5f9cc2c2c1cf7c8030071906058d4bd7e131
kimsuns <kimksy27@gmail.com>
2024-01-18 21:30:05 +0900

    Feat: 내비게이션바 버튼 추가
    - 링크 기능 추가 (클릭시 post 페이지 이동)

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import Logo from './Logo';
+
+export default function Header() {
+  return (
+    <div>
+      <h2>로고</h2>
+      <Nav>
+        <div>
+          <Logo />
+        </div>
+        <div>
+          <Link to="/post">
+            <Button>롤링 페이퍼 만들기</Button>
+          </Link>
+        </div>
+      </Nav>
+    </div>
+  );
+}
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  // gap: 944px;
+`;
+const Button = styled.button`
+  background-color: #6750a4;
+  border: none;
+  color: #ffffff;
+  padding: 16px;
+`;
---
+++ src/components/header/Logo.jsx
@@ -0,0 +1,14 @@
+import logoImg from '../../assetes/logoIcon.svg';
+
+function Logo() {
+  return (
+    <div>
+      <div>
+        <img src={logoImg} alt="Logo" />
+      </div>
+      <div>Rolling</div>
+    </div>
+  );
+}
+
+export default Logo;
