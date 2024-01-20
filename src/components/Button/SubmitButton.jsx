d4808111d56de907e3c6a0652c5e190caab684cf
Crack_Egg <jaemyung123@naver.com>
2024-01-20 23:48:19 +0900

    Feat: 생성하기 버튼 구현
    - 생성하기 버튼 구현
    - api 통신 이후 disabled 프롭스 추가 구현

---
+++ src/components/Button/SubmitButton.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export default function SubmitButton({ children, disabled, width }) {
+  return (
+    <ButtonStyle disabled={disabled} width={width}>
+      {children}
+    </ButtonStyle>
+  );
+}
+
+const ButtonStyle = styled.button`
+  font-size: 1.8rem;
+  font-weight: 700;
+  line-height: 2.8rem;
+  letter-spacing: -0.18px;
+  padding: 1.4rem 2.4rem;
+  border-radius: 1.2rem;
+  font-style: normal;
+  display: flex;
+  text-align: center;
+  color: var(--white, #fff);
+  border: none;
+  background: var(--purple-600, #9935ff);
+  cursor: pointer;
+
+  &:disabled {
+    background: var(--gray-300, #ccc);
+  }
+`;
