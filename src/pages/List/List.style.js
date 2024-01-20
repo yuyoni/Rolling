adc3b54d9efb34bec99bc62b9cf4821bed26c469
yuyoni <doli0116@naver.com>
2024-01-20 20:02:03 +0900

    Design : 전체 요소 중앙 정렬
    - Header 크기가 1920px 이라서 왼정렬됨 -> Wrapper로 감싸서 중앙정렬

--- src/pages/List/List.style.js
+++ src/pages/List/List.style.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
