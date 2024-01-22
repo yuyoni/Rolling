2ce27427fbed705edb2860d3e53f13cc4400c8e3
Crack_Egg <jaemyung123@naver.com>
2024-01-22 18:53:00 +0900

    Feat: 맵 함수를 이용한 프로필 이미지 요소 가져오기
    - map 함수를 사용하여 key, id 속성 지정
    - urls 스타일링 추가

--- src/components/ProfileImg/ProfileImgOption.jsx
+++ src/components/ProfileImg/ProfileImgOption.jsx
@@ -1,10 +1,14 @@
 import * as S from './ProfileImgOption.style';
 
-export default function ProfileImgOption() {
+export default function ProfileImgOption({ profileImages }) {
   return (
     <S.ProfileListContainer>
       <S.Subtitle>프로필 이미지를 선택해주세요!</S.Subtitle>
-      <S.ProfileList />
+      <S.ProfileList>
+        {profileImages.map((urls, index) => (
+          <S.ProfileOptionItem key={urls} $urls={urls} id={index} />
+        ))}
+      </S.ProfileList>
     </S.ProfileListContainer>
   );
 }
--- src/components/ProfileImg/ProfileImgOption.style.jsx
+++ src/components/ProfileImg/ProfileImgOption.style.jsx
@@ -20,3 +20,17 @@ export const ProfileList = styled.ul`
   flex-wrap: wrap;
   gap: 0.4rem;
 `;
+
+export const ProfileOptionItem = styled.li`
+  display: flex;
+  width: 5.6rem;
+  height: 5.6rem;
+  align-items: center;
+  flex-shrink: 0;
+  border-radius: 10rem;
+  border: 1px solid var(--gray-200, #eee);
+  background: ${({ $urls }) => ($urls ? `url(${$urls})` : '#fff')};
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
