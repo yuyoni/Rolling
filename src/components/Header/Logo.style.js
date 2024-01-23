f18afdda4c6eb49a50fe767c027318c45393bfdb
kimsuns <152264841+kimsuns@users.noreply.github.com>
2024-01-23 18:58:38 +0900

    Update Logo.style.js

--- src/components/Header/Logo.style.js
+++ src/components/Header/Logo.style.js
@@ -9,22 +9,13 @@ export const LogoStyle = styled.div`
 export const LogoText = styled.div`
   color: var(--gray-light-gray-90, #4a494f);
   text-align: center;
-<<<<<<< HEAD
   font-size: 20px;
   font-family: 'Pretendard';
   font-weight: 700;
+  
 `;
 
 export const LogoImage = styled.div`
-=======
-  font-size: 19.971px;
-  font-family: 'Pretendard-Bold';
-  line-height: normal;
-  font-weight: 700;
-`;
-
-export const LogoImg = styled.div`
->>>>>>> 91e6c49bcd60435fa7c5ab1e611961a21ebcb7a3
   width: 28px;
   height: 28px;
   fill: var(--primary-purple-50, #a64eff);
