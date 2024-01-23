b50706bd8126736995f5c37f7aaa877751199a7f
yuyoni <doli0116@naver.com>
2024-01-23 15:57:31 +0900

    Refactor : 유지보수를 위해 박스 타이틀 배열로 관리

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -2,11 +2,13 @@ import PaperBox from '../../components/Paper/PaperBox';
 import * as S from './List.style';
 
 export default function List() {
+  const boxTitles = ['인기 롤링 페이퍼 🔥', '최근에 만든 롤링 페이퍼 ⭐️️'];
   return (
     <S.Wrapper>
       <S.Container>
-        <PaperBox title="인기 롤링 페이퍼 🔥" />
-        <PaperBox title="최근에 만든 롤링 페이퍼 ⭐️️" />
+        {boxTitles.map(boxTitle => (
+          <PaperBox title={boxTitle} />
+        ))}
         <S.Button>나도 만들어 보기</S.Button>
       </S.Container>
     </S.Wrapper>
