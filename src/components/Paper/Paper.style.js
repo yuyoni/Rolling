93f6666774a3fbed6b2df245524bea4e41ee0b86
yuyoni <doli0116@naver.com>
2024-01-24 04:40:37 +0900

    Design : Paper에 마우스 호버 시 효과 + Paper 배경 색상 설정
    - 데이터에서 넘어오는 색상명이 purple이고 css 변수명이 Purple 이라서 불가피하게 복잡한 코드를 작성했습니다
    - 추후 다들 코드 다 작성하시고 난 후 리팩토링 때 복잡한 로직(대문자로 변경하는) 제거 후 사용 예정입니다

--- src/components/Paper/Paper.style.js
+++ src/components/Paper/Paper.style.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+/* color 부분 css 변수이름과 달라서 일시적으로 복잡하게 변환해놓음 - 추후 스타일 변수명 변경 제안 */
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -12,13 +13,17 @@ const Wrapper = styled.div`
     `${
       $backgroundImageURL
         ? `background-image: linear-gradient(rgba(255, 255, 255, 0.55), rgba(255, 255, 255, 0.55)), url(${$backgroundImageURL});`
-        : `background-color: ${$backgroundColor}`
+        : `background-color: var(--${$backgroundColor[0].toUpperCase() + $backgroundColor.slice(1)}-200)`
     }`};
   background-repeat: no-repeat;
   background-size: cover;
 
   box-shadow: 0 0.2rem 1.2rem 0 rgba(0, 0, 0, 0.08);
   padding: 2.4rem;
+
+  &:hover {
+    filter: brightness(0.8);
+  }
 
   @media (max-width: 360px) {
     width: 20.8rem;
