f560cf40943203eece778930bb50cf34a9ca19b9
yuyoni <doli0116@naver.com>
2024-01-25 01:41:39 +0900

    Feat : 에러 상태에 따라 보여줄 에러 컴포넌트 추가
    - Error 컴포넌트 추가해서 페이지에서 에러 발생 시 Error 컴포넌트 렌더링 하도록 추가

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -3,23 +3,28 @@ import PaperBox from '../../components/Paper/PaperBox';
 import Skeleton from '../../components/Paper/Skeleton/Skeleton';
 import useFetchData from '../../hooks/useFetchData';
 import * as S from './List.style';
+import Error from '../Error/Error';
 
 export default function List() {
-  const { data, isLoading } = useFetchData('recipients/');
+  const { data, isLoading, isError } = useFetchData('recipients/', 'GET');
+
+  if (isLoading) {
+    return <Skeleton />;
+  }
+
+  if (isError) {
+    return <Error />;
+  }
 
   return (
     <S.Wrapper>
-      {isLoading || !data ? (
-        <Skeleton />
-      ) : (
-        <S.Container>
-          <PaperBox orderBy="messageCount" paperData={data.results} />
-          <PaperBox orderBy="createdAt" paperData={data.results} />
-          <Link to="/post">
-            <S.Button>나도 만들어 보기</S.Button>
-          </Link>
-        </S.Container>
-      )}
+      <S.Container>
+        <PaperBox orderBy="messageCount" paperData={data.results} />
+        <PaperBox orderBy="createdAt" paperData={data.results} />
+        <Link to="/post">
+          <S.Button>나도 만들어 보기</S.Button>
+        </Link>
+      </S.Container>
     </S.Wrapper>
   );
 }
