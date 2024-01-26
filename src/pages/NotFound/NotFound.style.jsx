ecb9a5d491ccb3037d0fd8543d5c967dd9f4eafa
Crack_Egg <jaemyung123@naver.com>
2024-01-26 16:43:48 +0900

    Feat:NotFound 페이지 구현
    - 경로 수정 및 404 NotFound 페이지 구현

---
+++ src/pages/NotFound/NotFound.style.jsx
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+
+export const MainDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 2rem;
+  background: var(--Surface, #f6f8ff);
+`;
+
+export const WrapperDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem;
+  gap: 2rem;
+  background: #fff;
+  border-radius: 2rem;
+  color: var(--gray-500, #555);
+
+  @media screen and (min-width: 375px) {
+    width: 100%;
+  }
+
+  @media screen and (min-width: 76.8px) {
+    max-width: 76.8rem;
+  }
+
+  & img {
+    @media screen and (min-width: 375px) {
+      width: 18rem;
+    }
+
+    @media screen and (min-width: 768px) {
+      width: 28rem;
+    }
+  }
+
+  & h1 {
+    font-weight: 700;
+    color: transparent;
+    background: linear-gradient(15deg, #f6f8ff, #9966cc);
+    -webkit-background-clip: text;
+    background-clip: text;
+
+    @media screen and (min-width: 375px) {
+      font-size: 8rem;
+    }
+
+    @media screen and (min-width: 768px) {
+      font-size: 10rem;
+    }
+  }
+
+  & h2 {
+    text-align: center;
+
+    @media screen and (min-width: 375px) {
+      font-size: 2.3rem;
+    }
+
+    @media screen and (min-width: 768px) {
+      font-size: 3.2rem;
+    }
+  }
+`;
