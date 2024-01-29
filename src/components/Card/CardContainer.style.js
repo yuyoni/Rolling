107cb53ac82942dfc4907aaa2290ea01c9ccd250
Dev-Duke-Seo <dev.duke.seo@gmail.com>
2024-01-30 02:02:43 +0900

    Fix: 카드영역 grid 오류
    1) row 영역 사이즈 속성 추가

--- src/components/Card/CardContainer.style.js
+++ src/components/Card/CardContainer.style.js
@@ -6,6 +6,7 @@ export const CardContainer = styled.div`
   display: grid;
   gap: 2.4rem 2.8rem;
   grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(auto-fill, minmax(28rem, 28rem));
 `;
 
 export default CardContainer;
